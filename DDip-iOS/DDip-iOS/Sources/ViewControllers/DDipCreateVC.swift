45269fba632639a04b40666beb9347688345e332
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-11-20 22:30:48 +0900

    Inital Setting

---
+++ DDip-iOS/DDip-iOS/Sources/ViewControllers/DDipCreateVC.swift
@@ -0,0 +1,29 @@
+//
+//  DDipCreateVC.swift
+//  DDip-iOS
+//
+//  Created by SHIN YOON AH on 2021/11/20.
+//
+
+import UIKit
+
+class DDipCreateVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
