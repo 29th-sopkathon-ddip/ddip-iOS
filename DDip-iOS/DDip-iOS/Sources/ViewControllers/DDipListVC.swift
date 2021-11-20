92598002109c7f3de24855fd8b7add9b2fc4869e
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-11-21 01:09:00 +0900

    [ADD] NavigationController Embedded(#1)

--- DDip-iOS/DDip-iOS/Sources/ViewControllers/DDipListVC.swift
+++ DDip-iOS/DDip-iOS/Sources/ViewControllers/DDipListVC.swift
@@ -7,23 +7,20 @@
 
 import UIKit
 
-class DDipListVC: UIViewController {
+final class DDipListVC: BaseViewController {
+    
+    // MARK: - @IBOutlet
+    
+    
+    // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Override Methods
+    
+    override func configUI() {
+        super.configUI()
     }
-    */
-
 }
