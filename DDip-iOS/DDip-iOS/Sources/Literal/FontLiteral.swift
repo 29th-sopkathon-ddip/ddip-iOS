52f7ff9e53a5137e173902392d2b75a47e407743
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-11-21 00:42:42 +0900

    [ADD] Font 추가

--- DDip-iOS/DDip-iOS/Sources/Literal/FontLiteral.swift
+++ DDip-iOS/DDip-iOS/Sources/Literal/FontLiteral.swift
@@ -8,12 +8,9 @@
 import UIKit
 
 struct AppFontName {
-    static let thin = "NotoSansKR-Thin"
-    static let light = "NotoSansKR-Light"
-    static let regular = "NotoSansKR-Regular"
-    static let medium = "NotoSansKR-Medium"
-    static let bold = "NotoSansKR-Bold"
-    static let black = "NotoSansKR-Black"
+    static let light = "GmarketSansLight"
+    static let medium = "GmarketSansMedium"
+    static let bold = "GmarketSansBold"
 }
 
 extension UIFontDescriptor.AttributeName {
@@ -21,74 +18,19 @@ extension UIFontDescriptor.AttributeName {
 }
 
 extension UIFont {
-    @objc class func notoSansThinFont(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: AppFontName.thin, size: size)!
-    }
     
-    @objc class func notoSansLightFont(ofSize size: CGFloat) -> UIFont {
+    @objc class func gmarketLightFont(ofSize size: CGFloat) -> UIFont {
         return UIFont(name: AppFontName.light, size: size)!
     }
-    
-    @objc class func notoSansRegularFont(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: AppFontName.regular, size: size)!
-    }
-    
-    @objc class func notoSansMediumFont(ofSize size: CGFloat) -> UIFont {
+
+    @objc class func gmarketMediumFont(ofSize size: CGFloat) -> UIFont {
         return UIFont(name: AppFontName.medium, size: size)!
     }
     
-    @objc class func notoSansBoldFont(ofSize size: CGFloat) -> UIFont {
+    @objc class func gmarketBoldFont(ofSize size: CGFloat) -> UIFont {
         return UIFont(name: AppFontName.bold, size: size)!
     }
     
-    @objc class func notoSansBlackFont(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: AppFontName.black, size: size)!
-    }
-    
-    @objc convenience init(myCoder aDecoder: NSCoder) {
-        if let fontDescriptor = aDecoder.decodeObject(forKey: "UIFontDescriptor") as? UIFontDescriptor {
-            if let fontAttribute = fontDescriptor.fontAttributes[.nsctFontUIUsage] as? String {
-                var fontName = ""
-                switch fontAttribute {
-                case "CTFontRegularUsage":
-                    fontName = AppFontName.regular
-                case "CTFontEmphasizedUsage", "CTFontBoldUsage":
-                    fontName = AppFontName.bold
-                case "CTFontObliqueUsage":
-                    fontName = AppFontName.black
-                default:
-                    fontName = AppFontName.regular
-                }
-                self.init(name: fontName, size: fontDescriptor.pointSize)!
-            }
-            else {
-                self.init(myCoder: aDecoder)
-            }
-        }
-        else {
-            self.init(myCoder: aDecoder)
-        }
-    }
-    
-    class func overrideInitialize() {
-        if self == UIFont.self {
-            let systemFontMethod = class_getClassMethod(self, #selector(systemFont(ofSize:)))
-            let mySystemFontMethod = class_getClassMethod(self, #selector(notoSansThinFont(ofSize:)))
-            method_exchangeImplementations(systemFontMethod!, mySystemFontMethod!)
-            
-            let boldSystemFontMethod = class_getClassMethod(self, #selector(boldSystemFont(ofSize:)))
-            let myBoldSystemFontMethod = class_getClassMethod(self, #selector(notoSansBoldFont(ofSize:)))
-            method_exchangeImplementations(boldSystemFontMethod!, myBoldSystemFontMethod!)
-            
-            let italicSystemFontMethod = class_getClassMethod(self, #selector(italicSystemFont(ofSize:)))
-            let myItalicSystemFontMethod = class_getClassMethod(self, #selector(notoSansBlackFont(ofSize:)))
-            method_exchangeImplementations(italicSystemFontMethod!, myItalicSystemFontMethod!)
-            
-            let initCoderMethod = class_getInstanceMethod(self, #selector(UIFontDescriptor.init(coder:)))
-            let myInitCoderMethod = class_getInstanceMethod(self, #selector(UIFont.init(myCoder:)))
-            method_exchangeImplementations(initCoderMethod!, myInitCoderMethod!)
-        }
-    }
 }
 
 
