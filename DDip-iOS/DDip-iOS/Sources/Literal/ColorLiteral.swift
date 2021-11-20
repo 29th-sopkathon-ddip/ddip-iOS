945f4374911cb5cc41be37a29dc6a66c18599e8b
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-11-21 00:28:25 +0900

    [ADD] ColorLiteral 색 추가

--- DDip-iOS/DDip-iOS/Sources/Literal/ColorLiteral.swift
+++ DDip-iOS/DDip-iOS/Sources/Literal/ColorLiteral.swift
@@ -9,35 +9,22 @@ import UIKit
 
 extension UIColor {
     @nonobjc class var main: UIColor {
-        return UIColor(red: 0.914, green: 0.282, blue: 0.298, alpha: 1)
+        return UIColor(red: 80/255, green: 0, blue: 1, alpha: 1)
     }
     
-    @nonobjc class var colorSub1: UIColor {
-        return UIColor(red: 0.976, green: 0.89, blue: 0.294, alpha: 1)
+    @nonobjc class var sub1: UIColor {
+        return UIColor(red: 206/255, green: 1, blue: 0, alpha: 1)
     }
     
-    @nonobjc class var colorSub2: UIColor {
-        return UIColor(red: 0.929, green: 0.735, blue: 0.236, alpha: 1)
+    @nonobjc class var sub2: UIColor {
+        return UIColor(red: 1, green: 29/255, blue: 99/255, alpha: 1)
     }
     
-    @nonobjc class var colorSub3: UIColor {
-        return UIColor(red: 0.769, green: 0.769, blue: 0.769, alpha: 1)
+    @nonobjc class var sub3: UIColor {
+        return UIColor(red: 0, green: 1, blue: 182/255, alpha: 1)
     }
     
-    @nonobjc class var colorSub4: UIColor {
-        return UIColor(red: 0.538, green: 0.477, blue: 0.477, alpha: 1)
+    @nonobjc class var black: UIColor {
+        return UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1)
     }
-
-    @nonobjc class var colorSub5: UIColor {
-        return UIColor(red: 0.342, green: 0.296, blue: 0.296, alpha: 1)
-    }
-    
-    @nonobjc class var white: UIColor {
-        return UIColor(white: 1.0, alpha: 1.0)
-    }
-    
-    @nonobjc class var colorSubLightGray: UIColor {
-        return UIColor(red: 0.969, green: 0.969, blue: 0.969, alpha: 1)
-    }
-    
 }
