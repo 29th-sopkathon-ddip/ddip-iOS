5ac50e0e74d6c3a76be3356f933b23c6d0e7569a
loinsir <a9327370@gmail.com>
2021-11-21 03:31:47 +0900

    [ADD] 상단 바 작업

--- DDip-iOS/DDip-iOS/Sources/ViewControllers/DDipDetailVC.swift
+++ DDip-iOS/DDip-iOS/Sources/ViewControllers/DDipDetailVC.swift
@@ -7,23 +7,12 @@
 
 import UIKit
 
-class DDipDetailVC: UIViewController {
+class DDipDetailVC: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
